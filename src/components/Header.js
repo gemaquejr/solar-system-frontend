8fe52ee66a02854f4e090a34483c8f1d36b94939
gemaquejr <gemaquejr@hotmail.com>
2021-10-29 16:54:45 -0300

    Adiciona requisito 1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <Header>
+        <p>Sistema Solar</p>
+      </Header>
+    );
+  }
+}
+
+export default Header;
