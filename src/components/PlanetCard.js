e82c0e418adc057cc877e5c2962273ded66713a0
gemaquejr <gemaquejr@hotmail.com>
2021-11-02 21:17:38 -0300

    Adiciona requisitos 4, 5, 6 e inicio do 7

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <span data-testid="planetName">{planetName}</span>
+        <img src={ planetImage } alt={ `planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
