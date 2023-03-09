9b9524c4aecdd77394e2ccbbfbcb6c7f409491c7
gemaquejr <gemaquejr@hotmail.com>
2023-03-09 20:04:24 -0300

    Adiciona refatoracao do componente Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Title extends Component {
+class Title extends React.Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>{headline}</h2>
+      <h2>
+        { headline }
+      </h2>
     );
   }
 }
 
+export default Title;
+
 Title.propTypes = {
   headline: PropTypes.string.isRequired,
 };
-
-export default Title;
