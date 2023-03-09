958ce78c5365bdcf957ec67fc4e777cd3b2b70ad
gemaquejr <gemaquejr@hotmail.com>
2023-03-09 19:57:35 -0300

    Adiciona refatoracao do componente MissionCard.js

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class MissionCard extends Component {
+class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <ol>
-          <li data-testid="mission-name">{name}</li>
-          <li data-testid="mission-year">{year}</li>
-          <li data-testid="mission-country">{country}</li>
-          <li data-testid="mission-destination">{destination}</li>
-        </ol>
+      <div className="mission-card" data-testid="mission-card">
+        <h3 data-testid="mission-name">
+          {name}
+        </h3>
+        <p data-testid="mission-year">
+          {year}
+        </p>
+        <p data-testid="mission-country">
+          {country}
+        </p>
+        <p data-testid="mission-destination">
+          {destination}
+        </p>
       </div>
     );
   }
 }
+
+export default MissionCard;
 
 MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
@@ -23,5 +31,3 @@ MissionCard.propTypes = {
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
 };
-
-export default MissionCard;
