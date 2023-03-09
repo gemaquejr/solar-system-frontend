8456499400567688d2bfed4fa92160fc395c31e2
gemaquejr <gemaquejr@hotmail.com>
2023-03-09 20:01:28 -0300

    Adiciona refatoracao do componente PlanetCard.js

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-class PlanetCard extends Component {
+class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props;
+    const { planetImage, planetName } = this.props;
     return (
-      <div data-testid="planet-card">
-        <span data-testid="planet-name">{planetName}</span>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      <div className="planet-card" data-testid="planet-card">
+        <p className="planet-name" data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={planetImage} alt={`Planeta ${planetName}`} />
       </div>
     );
   }
 }
 
-PlanetCard.propTypes = {
-  planetImage: PropTypes.string.isRequired,
-  planetName: PropTypes.string.isRequired,
-};
-
 export default PlanetCard;
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
